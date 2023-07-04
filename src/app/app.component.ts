1495ea715a0f8c30bd7f61629916f19ede784a21
Fatma-Ozturk <fatmabuyukkozturk@gmail.com>
2023-07-04 16:17:01 +0300

    two-way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template:`<p>Hello world!</p>
+  <hr>
+  <div style= "background-color: aqua;">
+    <app-home></app-home>
+  </div>
+  //two-way data binding
+  <input type="text" [(ngModel)]="name"><br>
+  <input type="text" [(ngModel)]="name"><br>
+  {{name}}
+`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'FirstAngularProject';
+  name: string= "";
 }
