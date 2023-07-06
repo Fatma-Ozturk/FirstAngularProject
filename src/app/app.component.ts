4d2ff9daf4d2f244d6c8bb594999df7022f3d26b
Fatma-Ozturk <fatmabuyukkozturk@gmail.com>
2023-07-06 11:22:24 +0300

    Template-Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -31,11 +32,42 @@ import { Component } from '@angular/core';
     <li *appCustomFor="names; let isim; let idx = index">{{isim}} - {{idx}}</li>
   </ul>
   //Component Life Cycle Hook
-  <app-example data="merhaba">content</app-example>
+  <app-example data="merhaba">content</app-example><br>
+  <hr>
+  //Template-Drive Form
+  <form #frm="ngForm" (ngSubmit)="submitFonk(frm.value)">
+    <input type="text" name="name" placeholder="Name" ngModel><br>
+    <input type="text" name="surname" placeholder="Surname" ngModel><br>
+    <input type="email" name="email" placeholder="Email" ngModel><br>
+    <input type="tel" name="tel" placeholder="Tel"><br>
+    <div ngModelGroup="address">
+      <input type="text" name="country" placeholder="country" ngModel><br>
+      <input type="text" name="city" placeholder="city" ngModel><br>
+      <input type="text" name="address" placeholder="address" ngModel><br>
+    </div>
+
+    <button>Send</button>
+  </form>
 `,
 })
 export class AppComponent {
   title = 'FirstAngularProject';
   name: string = "";
   names: string[] = ["Fatma", "Özdemir", "Öztürk"];
+
+  @ViewChild("frm", { static: true })
+  frm!: NgForm;
+
+  submitFonk(data: any): void{
+    console.log(`Value: ${this.frm.value}`);
+    console.log(`Valid: ${this.frm.valid}`);
+    console.log(`Touched: ${this.frm.touched}`);
+    console.log(`Submitted: ${this.frm.submitted}`);
+    console.log(data);
+    console.log(this.frm);
+    console.log(this.frm.form);
+    console.log(this.frm.controls);
+
+    
+  }
 }
